4b904afc05e1088c4a681a6f24193fba0df4bedf
GxTeo <teog0015@e.ntu.edu.sg>
2024-07-06 00:59:18 +0800

    added comments for word frequency

--- word_frequency.py
+++ word_frequency.py
@@ -1,19 +1,20 @@
-
-
 if __name__ == '__main__':
 
     with open('pg16317.txt', "r") as f:
         text = f.read()
 
+    # Split the text into words
     words = text.split()
     word_frequency = {}
+
+    # Calculate the frequency of each word
     for word in words:
         if word in word_frequency:
             word_frequency[word] += 1
         else:
             word_frequency[word] = 1
 
-
+    # Sort the words by frequency in descending order
     sorted_frequency = sorted(word_frequency.items(), key=lambda x: x[1], reverse=True)
     print("Words ranked from 10th to 20th by frequency:")
     for i in range(9, 19):
